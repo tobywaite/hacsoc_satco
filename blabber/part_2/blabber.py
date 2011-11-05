bd49584d01a8be1fcb6282a37d09c653de7a40cf
Toby Waite <toby.waite@gmail.com>
2011-11-05 00:43:10 -0400

    the basic blabber app works

--- blabber/part_2/blabber.py
+++ blabber/part_2/blabber.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from datetime import datetime
+
+from flask import Flask, render_template, redirect, request, url_for, session
 import flaskext.sqlalchemy as sqla
-from flaskext.wtf import Form, TextField, Required
+from flaskext.wtf import Form, PasswordField, HiddenField, TextField, Required
+
+SECRET_KEY = 'dev_key' # in a real project, this should be random.
 
 # Create Flask application object
 app = Flask(__name__)
@@ -11,39 +15,79 @@ db = sqla.SQLAlchemy(app)
 
 # VIEWS #
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    user_id = session.get('user_id', None)
+    if user_id:
+        return redirect(url_for('home'))
+    
+    if request.method == 'GET':
+        login_form = LoginForm()
+        add_user_form = AddUserForm()
+        return render_template('login.html', login_form=login_form, add_user_form=add_user_form)
+    else:
+        form = LoginForm()
+        try:
+            user = User.query.filter_by(name=form.username.data).one()
+            if user.password == form.password.data:
+                session['user_id'] = user.id
+        except Exception, e:
+            print e
+
+        return redirect(url_for('home'))
+
+@app.route("/logout")
+def logout():
+    if session.get('user_id', None):
+        session.pop('user_id', None)
+    return redirect(url_for('home'))
+
 @app.route("/")
 def home():
-    if not session.user_id:
+    user_id = session.get('user_id', None)
+    if not user_id:
         return redirect(url_for('login'))
 
-
+    blabs = db.session.query(Blab). \
+        filter(Creep.creeper_id==user_id). \
+        filter(Blab.user_id==Creep.creepee_id).\
+        all()
     blab_form = BlabForm()
     
     return render_template('blabs.html', blabs=blabs, blab_form=blab_form)
 
-@app.route("/user/<user_id>")
-def show_user():
-    blabs = Blab.query.filter_by(Blab.user_id=user_id).all()
-    creeped_users = User.query.all() # TODO: This should return just the creeped users (join against creep)
-    return render_template('user.html', blabs=blabs, creeped_users=creeped_users)
+@app.route('/user/<user_id>')
+def show_user(user_id):
+    user = User.query.filter_by(id=user_id).one()
+    blabs = Blab.query.filter_by(user_id=user_id).all()
+    return render_template('user.html', blabs=blabs, user=user)
 
 @app.route("/users")
 def show_users():
-    users = Users.query.all()
-    return render_template('users.html', users=users)
+    users = User.query.all()
+    creeped_users = db.session.query(User). \
+        filter(User.id==Creep.creepee_id). \
+        filter(Creep.creeper_id==session['user_id']).all() 
+    return render_template('users.html', users=users, creeped_users=creeped_users)
 
 @app.route("/add_blab", methods=['POST'])
 def add_blab():
     form = BlabForm()
-    new_blab = Blab(form.blab)
+
+    user_id = session['user_id']
+    user = User.query.filter_by(id=user_id).one()
+  
+    new_blab = Blab(form.blab.data, user)
+    
     db.session.add(new_blab)
     db.session.commit()
+  
     return redirect(url_for('home'))
 
 @app.route("/add_user", methods=['POST'])
 def add_user():
-    form = UserForm()
-    new_user = User(form.username, form.password)
+    form = AddUserForm()
+    new_user = User(form.username.data, form.password.data)
     db.session.add(new_user)
     db.session.commit()
     return redirect(url_for('home'))
@@ -51,64 +95,70 @@ def add_user():
 @app.route("/creep_user", methods=['POST'])
 def creep_user():
     # don't do anything if we're not logged in.
-    if session.user_id:
+    user_id = session.get('user_id', None)
+    if user_id:
         form = CreepForm()
-        creepee_id = form.user_id
-        creepee = User.query.filter_by(id = creepee_id).one()
-        creeper_id = session.user_id
-        creeper = User.query.filter_by(id = creeper_id).one()
+        creepee_id = form.user_id.data
+        creeper_id = user_id
 
-        new_creep = Creep(creepee, creeper)
+        new_creep = Creep(creeper_id, creepee_id)
         db.session.add(new_creep)
         db.session.commit()
 
-    return redirect(url_for('home'))
+    return redirect(url_for('show_users'))
 
 @app.route("/uncreep_user", methods=['POST'])
 def uncreep_user():
     # don't do anything if we're not logged in.
-    if session.user_id:
+    user_id = session.get('user_id', None)
+    if user_id:
         form = CreepForm()
         try:
-            creep = Creep.query.filter_by(creeper_id=session.user_id, creepee_id=form.user_id).one()
+            creep = Creep.query.filter_by(creeper_id=user_id, creepee_id=form.user_id.data).one()
             db.session.delete(creep)
+            db.session.commit()
         except Exception, e:
             print e
 
-    return redirect(url_for('home'))
+    return redirect(url_for('show_users'))
 
 # FORMS #
 
-def BlabForm(Form):
-    blab = TextField(blab, validators=[Required()])
+class BlabForm(Form):
+    blab = TextField('blab', validators=[Required()])
 
-def CreepForm(Form):
-    user_id = TextField(blab, validators=[Required()])
+class LoginForm(Form):
+    username = TextField('username', validators=[Required()])
+    password = PasswordField('password', validators=[Required()])
 
-def UnCreepForm(Form):
-    user_id = TextField(blab, validators=[Required()])
+class AddUserForm(Form):
+    username = TextField('username', validators=[Required()])
+    password = TextField('password', validators=[Required()])
 
-def UserForm(Form):
-    username = TextField(username, validators=[Required()])
-    password = TextField(password, validators=[Required()])
+class CreepForm(Form):
+    user_id = HiddenField('user_id', validators=[Required()])
+
+class UnCreepForm(Form):
+    user_id = HiddenField('user_id', validators=[Required()])
 
 # MODELS #
 
-def Blab(db.Model):
+class Blab(db.Model):
     id = db.Column(db.Integer, primary_key=True)
    
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    owner = db.relationship('User',
+    user = db.relationship('User',
         backref=db.backref('blab', lazy='dynamic'))
 
     time = db.Column(db.DateTime)
     text = db.Column(db.String())
 
-    def __init__(self, text, time=datetime.utcnow()):
+    def __init__(self, text, user, time=datetime.utcnow()):
+        self.user = user
         self.time = time
         self.text = text
 
-def User(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     password = db.Column(db.String(80)) # THIS PASSWORD IS STORED IN PLAIN TEXT. THIS IS BAD.
@@ -117,18 +167,27 @@ def User(db.Model):
         self.name = name
         self.password = password
 
-def Creep(db.Model):
+    def __repr__(self):
+        return "<User Obj: %s>" % self.name
+
+class Creep(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     creeper_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    creeper = db.relationship('User',
-        backref=db.backref('creep', lazy='dynamic'))
-
     creepee_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    creepee = db.relationship('User',
-        backref=db.backref('creep', lazy='dynamic'))
+
+    def __init__(self, creeper_id, creepee_id):
+        self.creeper_id = creeper_id
+        self.creepee_id = creepee_id
+
+    def __repr__(self):
+        creeper = User.query.filter_by(id=self.creeper_id).one()
+        creepee = User.query.filter_by(id=self.creepee_id).one()
+        return "<Creep Obj: %s is creepin on %s>" % (creeper.name, creepee.name)
 
 # OTHER #
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0.', port='8080')
+    db.create_all()
+    app.secret_key = SECRET_KEY
+    app.run(host='0.0.0.0.', port=8080, debug=True)
